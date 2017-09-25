d6632731dc8b8dc8ed4b8f0c336e492b1c9bde15
NikthaFish <fischern93@gmail.com>
2017-09-25 10:32:23 -0500

    Form returns rotated string inside text box

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from flask import Flask, request
-from ceasar.py import rotate_string
+from ceasar import rotate_string
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
@@ -10,27 +10,30 @@ form = """
 <html>
     <head>
         <style>
-            form {
+            form {{
                 background-color: #eee;
                 padding: 20px;
                 margin: 0 auto;
                 width: 540px;
                 font: 16px sans-serif;
                 border-radius: 10px;
-            }
-            textarea {
+            }}
+            textarea {{
                 margin: 10px 0;
                 width: 540px;
                 height: 120px;
-            }
+            }}
         </style>
     </head>
     <body>
         <form method = "post">
         <label for "rot_number">Rotate by:</label>
-        <input id = "rot_number" type = "text" name = "rot" value = "0" autofocus/><br>
+        <input id = "rot_number" type = "text" name = "rot_number" value = "" autofocus/><br>
         <label>
-            <textarea name = "text"></textarea>
+            <textarea 
+                name = "text_box">
+                {text}
+            </textarea>
         </label>
         <input type = "submit"/>
     </body>
@@ -39,11 +42,14 @@ form = """
 
 @app.route("/")
 def index():
-    return form
+    return form.format(text = 'Enter your text here')
 
 @app.route("/", methods = ['POST'])
 def encrpyt():
+    rot = request.form['rot_number']
+    text = request.form['text_box']
+    rot = int(rot)
+    content = rotate_string(text, rot)
+    return form.format(text=content)
 
-
-    
 app.run()
